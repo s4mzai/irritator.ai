c6be37edb2e6357b59baf6f7184880e12b0268bf
samar <hi.samar.7x@gmail.com>
2025-10-13 19:03:05 +0530

    Add Logo

--- irritator.ai-frontend/src/App.tsx
+++ irritator.ai-frontend/src/App.tsx
@@ -5,9 +5,14 @@ import Home from './pages/Home'
 
 function App() {
   return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <div className='relative'>
+        <img className='w-15 h-15 absolute top-5 left-5 md:left-10' src="/irritator-dark.png" alt="" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
