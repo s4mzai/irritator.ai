5f29909f85a36a26164ecae65554abaf5651170b
samar <hi.samar.7x@gmail.com>
2025-10-13 16:04:19 +0530

    Add backend server for code explain

--- irritator.ai-backend/index.js
+++ irritator.ai-backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import rateLimit from 'express-rate-limit';
+import { GoogleGenAI } from "@google/genai";
 
 dotenv.config();
 
@@ -24,9 +25,46 @@ const limiter = rateLimit({
   
 app.use(limiter);
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('hello');
+
+const GEMINI_API_KEY=process.env.GEMINI_API_KEY;
+
+const gemini = new GoogleGenAI(GEMINI_API_KEY);
+
+app.post("/api/explain-code", async (req, res) => {
+
+    const explainLengths = {
+        "small": "200-250",
+        "medium": "700-800",
+        "large": "1000-1200"
+    }
+    try {
+      const { code, language, explainCharLength } = await req.body;
+      
+      if (!code) {
+        return res.status(400).json({ error: "Code is required" });
+      }
+      
+      const lengthRange = explainLengths[explainCharLength] || explainLengths["medium"];
+    
+      const response = await gemini.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents:`Please explain this ${language || ""} code in simple terms, in ${lengthRange} characters :\n\n\`\`\`${language || ""}\n${code}\n\`\`\``,
+        temperature: 0.9,
+        topP: 1,
+        topK: 1,
+        maxOutputTokens: 800,
+      });
+  
+      const explanation = response.text
+      if (!explanation) {
+        return res.status(500).json({ error: "Failed to explain code" });
+      }
+  
+      res.json({ explanation, language: language || "unknown" });
+    } catch (err) {
+      console.error("Code Explain API Error:", err);
+      res.status(500).json({ error: "Server error", details: err.message });
+    }
 });
   
 const PORT = process.env.PORT || 3000;
