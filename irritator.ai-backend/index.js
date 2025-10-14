4523143d3a05441728bf098ab1d43719eb614fb7
samar <hi.samar.7x@gmail.com>
2025-10-14 19:53:49 +0530

    import frontend url from the env

--- irritator.ai-backend/index.js
+++ irritator.ai-backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.FRONTEND_URL, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
